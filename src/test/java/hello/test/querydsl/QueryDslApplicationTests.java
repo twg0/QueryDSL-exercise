5947d82e6218109bfa9fab048301c1b7d5a7622b
twg0 <fruitsjh@gmail.com>
2023-06-12 09:23:22 +0900

    first project setup

---
+++ src/test/java/hello/test/querydsl/QueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.test.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
