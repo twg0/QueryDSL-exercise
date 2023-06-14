4c3d002950bc1b0f3ad7b4392a25e78e645ea04b
twg0 <fruitsjh@gmail.com>
2023-06-15 00:08:51 +0900

    feat : OrderSearch 구현

---
+++ src/main/java/hello/test/querydsl/domain/OrderSearch.java
@@ -0,0 +1,12 @@
+package hello.test.querydsl.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+
+}
