52f9d7d88328ba0211d5011471f2c960e6963085
twg0 <fruitsjh@gmail.com>
2023-06-15 13:34:44 +0900

    feat : db 초기값 할당 및 주문 조회 V1을 위한 의존성 설정

--- src/main/java/hello/test/querydsl/QueryDslApplication.java
+++ src/main/java/hello/test/querydsl/QueryDslApplication.java
@@ -1,11 +1,17 @@
 package hello.test.querydsl;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QueryDslApplication {
 
+//    @Bean
+//    Hibernate5JakartaModule hibernate5Module() {
+//        return new Hibernate5JakartaModule();
+//    }
     public static void main(String[] args) {
         SpringApplication.run(QueryDslApplication.class, args);
     }
