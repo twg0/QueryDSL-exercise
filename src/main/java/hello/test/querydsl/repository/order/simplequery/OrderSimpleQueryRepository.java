a5aa441fae2caa4e4d54d8ec5b07a3fc2aa01037
twg0 <fruitsjh@gmail.com>
2023-06-16 22:21:20 +0900

    feat : OrderSimpleQueryRepository 구현

---
+++ src/main/java/hello/test/querydsl/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -0,0 +1,23 @@
+package hello.test.querydsl.repository.order.simplequery;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderSimpleQueryRepository {
+
+    private final EntityManager em;
+
+    private List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery("select new hello.test.querydsl.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                        " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+
+}
