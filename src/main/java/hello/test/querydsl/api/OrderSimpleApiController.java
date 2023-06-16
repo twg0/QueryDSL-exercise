af8fadae139e6021e138134e600fd9e116febdb0
twg0 <fruitsjh@gmail.com>
2023-06-16 22:20:29 +0900

    feat : OrderSimpleApiController V2 ~ V3

--- src/main/java/hello/test/querydsl/api/OrderSimpleApiController.java
+++ src/main/java/hello/test/querydsl/api/OrderSimpleApiController.java
@@ -1,21 +1,26 @@
 package hello.test.querydsl.api;
 
+import hello.test.querydsl.domain.Address;
 import hello.test.querydsl.domain.Order;
 import hello.test.querydsl.domain.OrderSearch;
+import hello.test.querydsl.domain.OrderStatus;
 import hello.test.querydsl.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /**
- *
  * xToOne(ManyToOne, OneToOne) 관계 최적화
  * Order
  * Order -> Member
  * Order -> Delivery
- *
  */
 @RestController
 @RequiredArgsConstructor
@@ -37,5 +42,51 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     * 참고: fetch join에 대한 자세한 내용은 JPA 기본편 참고(정말 중요함)
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map( o -> new SimpleOrderDto(o))
+                .collect(toList());
+
+        return result;
     }
 }
